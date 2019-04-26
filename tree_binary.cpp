14240e29082ea39122d48409aa85351d8fa48995
Vladimir Popov <vladimir.popov@die-interaktiven.de>
2019-04-26 17:10:25 +0300

    Edited deleteNode(). Added displayTree()

--- tree_binary.cpp
+++ tree_binary.cpp
@@ -1,255 +1,307 @@
 #include <iostream>
+#include <vector>
 
 class Tree
 {
 private:
-	class Node;
+    class Node;
 
 public:
-	Tree() : m_root(nullptr) {};
-	~Tree()
-	{
-		if (!m_root) {
-			delete m_root;
-		}
-	}
+    Tree() : m_root(nullptr) {};
+    ~Tree()
+    {
+        if (!m_root) {
+            deleteTree(m_root);
+        }
+    }
 
-	void insert(const int& t_key, const int& t_value);
-	void find(const int& t_key);
-	void deleteNode(const int& t_key);
-	void inorder(const Node* t_node);
-	void printInOrder();
+    void insert(const int& t_key, const int& t_value);
+    void find(const int& t_key);
+    void deleteNode(const int& t_key);
+    void inorder(const Node* t_node);
+    void deleteTree(const Node* t_node);
+    void printInOrder();
+    void displayTree();
 
 private:
-	class Node {
-	public:
-		Node(const int& t_key, const int& t_value) 
-		 : key(t_key), value(t_value), leftChild(nullptr), rightChild(nullptr)
-		{}
+    class Node {
+    public:
+        Node(const int& t_key, const int& t_value) 
+         : key(t_key), value(t_value), leftChild(nullptr), rightChild(nullptr)
+        {}
 
-		~Node() 
-		{
-			if (leftChild) {
-				delete leftChild;
-			}
+        ~Node() {}
 
-			if (rightChild) {
-				delete rightChild;
-			}
-		}
+        int key;
+        int value;
+        Node *leftChild;
+        Node *rightChild;
+    };
 
-		int key;
-		int value;
-		Node *leftChild;
-		Node *rightChild;
-	};
-
-	Node *findNode(const int& t_key);
-	
-	Node *m_root;
+    Node *findNode(const int& t_key);
+    
+    Node *m_root;
 };
 
 void Tree::find(const int& t_key)
 {
-	Node *foundNode = findNode(t_key);
+    Node *foundNode = findNode(t_key);
 
-	if (!foundNode) {
-		std::cout << "Node with key \"" << t_key << "\" wasn\'t found." << std::endl;
-	} else {
-		std::cout << "Key: " << foundNode->key << " Value: " << foundNode->value << std::endl;
-	}
+    if (!foundNode) {
+        std::cout << "Node with key \"" << t_key << "\" wasn\'t found." << std::endl;
+    } else {
+        std::cout << "Key: " << foundNode->key << " Value: " << foundNode->value << std::endl;
+    }
 }
 
 Tree::Node* Tree::findNode(const int& t_key)
 {
-	if (!m_root) {
-		return nullptr;
-	} else {
-		Node *current = m_root;
+    if (!m_root) {
+        return nullptr;
+    } else {
+        Node *current = m_root;
 
-		while (current->key != t_key) {
-			if (t_key < current->key) {
-				current = current->leftChild;
-			} else {
-				current = current->rightChild;
-			}
+        while (current->key != t_key) {
+            if (t_key < current->key) {
+                current = current->leftChild;
+            } else {
+                current = current->rightChild;
+            }
 
-			if (!current) {
-				return nullptr;
-			}
-		}
+            if (!current) {
+                return nullptr;
+            }
+        }
 
-		return current;
-	}
+        return current;
+    }
 }
 
 void Tree::insert(const int& t_key, const int& t_value)
 {
-	Node *newNode = new Node(t_key, t_value);
+    Node *newNode = new Node(t_key, t_value);
 
-	if (!m_root) {
-		m_root = newNode;
-		return;
-	} else {
-		Node *current = m_root;
-		Node *parent = nullptr;
+    if (!m_root) {
+        m_root = newNode;
+        return;
+    } else {
+        Node *current = m_root;
+        Node *parent = nullptr;
 
-		while (true) {
-			parent = current;
+        while (true) {
+            parent = current;
 
-			if (t_key < current->key) {
-				current = current->leftChild;
+            if (t_key < current->key) {
+                current = current->leftChild;
 
-				if (!current) {
-					parent->leftChild = newNode;
-					return;
-				}
-			} else {
-				current = current->rightChild;
+                if (!current) {
+                    parent->leftChild = newNode;
+                    return;
+                }
+            } else {
+                current = current->rightChild;
 
-				if (!current) {
-					parent->rightChild = newNode;
-					return;
-				}
-			}
-		}
-	}
+                if (!current) {
+                    parent->rightChild = newNode;
+                    return;
+                }
+            }
+        }
+    }
 }
 
 void Tree::deleteNode(const int& t_key)
 {
-	if (!m_root) {
-		std::cout << "Tree is empty." << std::endl;
-	} else {
-		Node *nodeToDelete = m_root;
-		Node *parent = nullptr;
-		bool isLeftChild = false;
+    if (!m_root) {
+        std::cout << "Tree is empty." << std::endl;
+    } else {
+        Node *nodeToDelete = m_root;
+        Node *parent = m_root;
+        bool isLeftChild = false;
 
-		while (nodeToDelete->key != t_key) {
-			parent = nodeToDelete;
+        while (nodeToDelete->key != t_key) {
+            parent = nodeToDelete;
 
-			if (t_key < nodeToDelete->key) {
-				nodeToDelete = nodeToDelete->leftChild;
-				isLeftChild = true;
-			} else {
-				nodeToDelete = nodeToDelete->rightChild;
-				isLeftChild = false;
-			}
+            if (t_key < nodeToDelete->key) {
+                nodeToDelete = nodeToDelete->leftChild;
+                isLeftChild = true;
+            } else {
+                nodeToDelete = nodeToDelete->rightChild;
+                isLeftChild = false;
+            }
 
-			if (!nodeToDelete) {
-				std::cout << "No node with key \"" << t_key << "\" to delete." << std::endl;
-				return; 
-			}
-		}
+            if (!nodeToDelete) {
+                std::cout << "No node with key \"" << t_key << "\" to delete." << std::endl;
+                return; 
+            }
+        }
 
-		if (!nodeToDelete->leftChild && !nodeToDelete->rightChild) {
-			if (isLeftChild) {
-				parent->leftChild = nullptr;
-			} else {
-				parent->rightChild = nullptr;
-			}
-		} else if (nodeToDelete->leftChild) {
-			if (isLeftChild) {
-				parent->leftChild = nodeToDelete->leftChild;
-			} else {
-				parent->rightChild = nodeToDelete->leftChild;
-			}
-		} else if (nodeToDelete->rightChild) {
-			if (isLeftChild) {
-				parent->leftChild = nodeToDelete->rightChild;
-			} else {
-				parent->rightChild = nodeToDelete->rightChild;
-			}
-		} else {
-			Node *successorParent = nodeToDelete;
-			Node *successor = nodeToDelete;
-			Node *current = nodeToDelete->rightChild;
+        if (!nodeToDelete->leftChild && !nodeToDelete->rightChild) {
+        	if (nodeToDelete == m_root) {
+        		delete nodeToDelete;
+        		m_root = nullptr;
+        		return;
+        	}
 
-			while (current->leftChild) {
-				successorParent = successor;
-				successor = current;
-				current = current->leftChild;
-			}
+            if (isLeftChild) {
+                parent->leftChild = nullptr;
+            } else {
+                parent->rightChild = nullptr;
+            }
+        } else if (!nodeToDelete->leftChild) {
+        	if (nodeToDelete == m_root) {
+        		m_root = nodeToDelete->leftChild;
+        		return;
+        	}
 
-			if (successor != nodeToDelete->rightChild) {
-				successorParent->leftChild = successor->rightChild;
-				successor->rightChild = nodeToDelete->rightChild;
-			}
+            if (isLeftChild) {
+                parent->leftChild = nodeToDelete->leftChild;
+            } else {
+                parent->rightChild = nodeToDelete->leftChild;
+            }
+        } else if (!nodeToDelete->rightChild) {
+        	if (nodeToDelete == m_root) {
+        		m_root = nodeToDelete->rightChild;
+        		return;
+        	}
 
-			if (isLeftChild) {
-				parent->leftChild = successor;
-			} else {
-				parent->rightChild = successor;
-			}
+            if (isLeftChild) {
+                parent->leftChild = nodeToDelete->rightChild;
+            } else {
+                parent->rightChild = nodeToDelete->rightChild;
+            }
+        } else {
+        	if (nodeToDelete == m_root) {
+        		deleteTree(m_root);
+        		return;
+        	}
 
-			successor->leftChild = nodeToDelete->leftChild;
-		}
+            Node *successorParent = nodeToDelete;
+            Node *successor = nodeToDelete;
+            Node *current = nodeToDelete->rightChild;
 
-		delete nodeToDelete;
-	}
+            while (current) {
+                successorParent = successor;
+                successor = current;
+                current = current->leftChild;
+            }
+
+            if (successor != nodeToDelete->rightChild) {
+                successorParent->leftChild = successor->rightChild;
+                successor->rightChild = nodeToDelete->rightChild;
+            }
+
+            if (isLeftChild) {
+                parent->leftChild = successor;
+            } else {
+                parent->rightChild = successor;
+            }
+
+            successor->leftChild = nodeToDelete->leftChild;
+        }
+
+        delete nodeToDelete;
+    }
 }
 
 void Tree::inorder(const Node* t_node)
 {
-	if (!t_node) {
-		return;
-	} else {
-		inorder(t_node->leftChild);
-		std::cout << t_node->key << std::endl;
-		inorder(t_node->rightChild);
-	}
+    if (!t_node) {
+        return;
+    } else {
+        inorder(t_node->leftChild);
+        std::cout << t_node->key << std::endl;
+        inorder(t_node->rightChild);
+    }
 }
+
+void Tree::deleteTree(const Node* t_node)
+{
+    if (!t_node) {
+        return;
+    } else {
+        deleteTree(t_node->leftChild);
+        Node *rightChild = t_node->rightChild;
+        delete t_node;
+        deleteTree(rightChild);
+    }
+}
+
 
 void Tree::printInOrder() {
-	inorder(m_root);
+    inorder(m_root);
 }
 
+void Tree::displayTree()
+{
+    std::vector<Node *> globalStackNodes;
+    globalStackNodes.push_back(m_root);
+    int numberOfBlanks = 32;
+    bool isRowEmpty = false;
+
+    while (!isRowEmpty) {
+        std::vector<Node *> localStackNodes;
+        isRowEmpty = true;
+
+        for (int i = 0; i < numberOfBlanks; ++i) {
+            std::cout << ' ';
+        }
+
+        while (!globalStackNodes.empty()) {
+            Node *temp = globalStackNodes.back();
+            globalStackNodes.pop_back();
+
+            if (temp) {
+            	std::cout << temp->key;
+            	localStackNodes.push_back(temp->leftChild);
+            	localStackNodes.push_back(temp->rightChild);
+
+            	if (temp->leftChild || temp->rightChild) {
+                    isRowEmpty = false;
+            	}
+            } else {
+            	std::cout << "--";
+            	localStackNodes.push_back(nullptr);
+            	localStackNodes.push_back(nullptr);
+            }
+
+            for (int i = 0; i < numberOfBlanks*2-2; ++i) {
+	            std::cout << ' ';
+	        }
+        }  
+
+        std::cout << std::endl;
+	    numberOfBlanks /= 2;
+
+	    while (!localStackNodes.empty()) {
+	      	globalStackNodes.push_back(localStackNodes.back());
+	       	localStackNodes.pop_back();
+	    }      
+    }
+}
 
 int main()
 {
-	Tree tree;
+    Tree tree;
 
-	tree.insert(20, 20);
-	tree.insert(10, 10);
-	tree.insert(5, 5);
-	tree.insert(15, 15);
+    tree.insert(20, 20);
+    tree.insert(10, 10);
+    tree.insert(5, 5);
+    tree.insert(15, 15);
     tree.insert(25, 25);
     tree.insert(22, 22);
     tree.insert(30, 30);
+    tree.insert(27, 27);
     tree.insert(35, 35);
     tree.insert(33, 33);
     tree.insert(34, 34);
     tree.insert(37, 37);
 
-	tree.find(20);
-	tree.find(10);
-	tree.find(5);
-	tree.find(15);
-    tree.find(25);
-    tree.find(22);
-    tree.find(30);
-    tree.find(35);
-    tree.find(33);
-    tree.find(34);
-    tree.find(37);
-	tree.find(0);
+    tree.displayTree();
 
-	tree.deleteNode(30);
+    tree.deleteNode(30);
 
-	tree.find(20);
-	tree.find(10);
-	tree.find(5);
-	tree.find(15);
-    tree.find(25);
-    tree.find(22);
-    tree.find(30);
-    tree.find(35);
-    tree.find(33);
-    tree.find(34);
-    tree.find(37);
-	tree.find(0);
+    tree.displayTree();
 
-	tree.printInOrder();
-
-	return 0;
+    return 0;
 }
